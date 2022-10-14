1aa25f41ab424404fb7ea5eaaeb4145172d78dad
Vadim <avkhimen@ualberta.ca>
2022-10-14 15:50:41 +0000

    Adding python script and jupyter notebook for creating percentages of canadians with higher than 50 mbps broadband speeds

--- notebooks/timeseries_analysis_ookla_data.py
+++ notebooks/timeseries_analysis_ookla_data.py
@@ -30,21 +30,29 @@ def make_next_year_next_quarter(year, quarter):
 
 def make_features_table(year, quarter):
     
+    connection_type = 'fixed' #'mobil'
+    
     ookla_tiles = ookla.canada_tiles()
     ookla_tiles['quadkey'] = ookla_tiles['quadkey'].astype(int)
+    
     da_pops = statcan.dissemination_areas_populations()
+    
+    da_pops = statcan.dissemination_areas_populations()
+    
     o = gp.read_file(src.config.OVERLAYS_DIR / 'tile_das_overlay') #this can take a few minutes to load.
     tile_da_label = o.dropna(subset=['DAUID','quadkey']).sort_values(by=['quadkey','tile_frac'],ascending=False).drop_duplicates(subset='quadkey', keep='first')
     tile_da_label['quadkey'] = tile_da_label['quadkey'].astype(int)
     tile_da_label['DAUID'] = tile_da_label['DAUID'].astype(int)
+    
     #last_4_quarters = ookla.speed_data(ookla.available_files().loc[('fixed',2021,3):('fixed',2022,2)].path)
     next_year, next_quarter = make_next_year_next_quarter(year, quarter)
-    last_4_quarters = ookla.speed_data(ookla.available_files().loc[('fixed',year,quarter):('fixed',next_year,next_quarter)].path)
+    last_4_quarters = ookla.speed_data(ookla.available_files().loc[(connection_type,year,quarter):(connection_type,next_year,next_quarter)].path)
     down = last_4_quarters.groupby('quadkey').apply(lambda s:np.average(s.avg_d_kbps, weights=s.tests)).rename('avg_d_kbps')
     up = last_4_quarters.groupby('quadkey').apply(lambda s:np.average(s.avg_u_kbps, weights=s.tests)).rename('avg_u_kbps')
     tests = last_4_quarters.groupby('quadkey')['tests'].sum()
     devices = last_4_quarters.groupby('quadkey')['devices'].sum()
     last4_agg = pd.concat([down, up, tests, devices],axis=1)
+    
     features_table = tile_da_label.merge(da_pops, on='DAUID', how='left')
     features_table['DAPOP'] = features_table['DAPOP'].fillna(0).astype(int)
     del features_table['GEO_NAME']
@@ -52,14 +60,17 @@ def make_features_table(year, quarter):
     del features_table['geometry']
     features_table['POP_DENSITY'] = features_table['DAPOP']/features_table['das_area']*1000**2 #people per square kilometer
     features_table = ookla_tiles.merge(last4_agg, on='quadkey').merge(features_table, on='quadkey')
+    
     pop_info = statcan.boundary('population_centres').to_crs('epsg:4326')
     pop_info = pop_info[['PCUID', 'PCNAME', 'PCTYPE', 'PCPUID', 'PCCLASS', 'geometry']] ##removes some redundant cols from DAs
     features_table = features_table.sjoin(pop_info, how='left')
     del features_table['index_right']
     features_table = features_table.sort_values(by=['PCUID','quadkey']).drop_duplicates(subset=['quadkey']) #keep tiles where overlap was true
     
+    features_table = features_table[features_table['tile_frac'] > 0.35] #may need to delete
+    
     #Add population column
-    features_table['population_in_tile'] = features_table['tile_area'] * features_table['POP_DENSITY']/1000000
+    features_table['population_in_tile'] = (features_table['tile_area'] * features_table['POP_DENSITY'] / features_table['tile_frac'])/1000000
     
     return features_table
 
@@ -69,7 +80,9 @@ if __name__ == "__main__":
     
     #Calculate total number of people
     features_table = make_features_table(2022,3)
-    total_people = sum(features_table['POP_DENSITY'] * features_table['tile_area'])/1000000
+    total_people = sum(features_table['POP_DENSITY'] * features_table['tile_area'] / features_table['tile_frac'])/1000000
+    
+    print('total_people', total_people)
     
     del features_table
     
@@ -88,14 +101,16 @@ if __name__ == "__main__":
 
             print(year, 'year', quarter, 'quarter')
             
-            internet_speed_type = 'avg_d_kbps' #'avg_u_kbps'
+            internet_speed_type = 'avg_u_kbps' #'avg_d_kbps'
+            
+            internet_speed = 10 #50
 
             features_table = make_features_table(year, quarter)
 
             #'POP_DENSITY' > 50
-            ts_greater_than_50.append(features_table[features_table[internet_speed_type] > 50*1000]['population_in_tile'].sum()/total_people)
+            ts_greater_than_50.append(features_table[features_table[internet_speed_type] > internet_speed*1000]['population_in_tile'].sum()/total_people)
             #'POP_DENSITY' < 50
-            ts_less_than_50.append(features_table[features_table[internet_speed_type] < 50*1000]['population_in_tile'].sum()/total_people)
+            ts_less_than_50.append(features_table[features_table[internet_speed_type] < internet_speed*1000]['population_in_tile'].sum()/total_people)
             # #'POP_DENSITY' > 50
             # gdf = features_table[(features_table['POP_DENSITY'] <= 1000) & (features_table['POP_DENSITY'] >= 50)]
             # ts_50.append(gdf['population_in_tile'].sum()/total_people)
